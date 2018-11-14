29898c8386ca8f43a69ae2547b774496557d706f
Sean Price <seanprice@sevenhillstechnology.com>
2018-11-14 16:39:35 -0500

    Add subscription methods to localStorageService to listen for changes made directly to local storage

--- src/services/localStorageService.ts
+++ src/services/localStorageService.ts
@@ -2,6 +2,7 @@ export class LocalStorageService {
     static readonly instance = new LocalStorageService();
 
     localStorageKeyName = "reperioCoreJWT";
+    subscribers: ((jwt: string, source: "internal" | "external") => any)[] = [];
 
     private constructor() {}
 
@@ -25,9 +26,39 @@ export class LocalStorageService {
             } else {
                 localStorage.removeItem(this.localStorageKeyName);
             }
+            this.dispatchToSubscribers("internal");
         } catch (e) {
             console.error(e);
         }
+    }
+
+    subscribe(method: (jwt: string, source: "internal" | "external") => any) {
+        if (this.subscribers.includes(method)) {
+            return;
+        }
+        this.subscribers.push(method);
+    }
+
+    unsubscribe(method: (jwt: string) => any) {
+        if (!this.subscribers.includes(method)) {
+            return;
+        }
+        this.subscribers.splice(this.subscribers.indexOf(method));
+    }
+
+    dispatchToSubscribers(source: "internal" | "external") {
+        const jwt = this.getReperioCoreJWT();
+        this.subscribers.forEach(x => x(jwt, source));
+    }
+
+    startEventListener() {
+        window.addEventListener("storage", e => {
+            if (e.storageArea !== localStorage || e.key !== this.localStorageKeyName) {
+                return;
+            }
+
+            this.dispatchToSubscribers("external");
+        });
     }
 }
 
