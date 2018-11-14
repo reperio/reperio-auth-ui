ed9a353762cb835191434d5ed3c0e3f75f5a155e
Sean Price <seanprice@sevenhillstechnology.com>
2018-11-14 16:25:19 -0500

    Make observeStore more generic

--- src/store/observeStore.ts
+++ src/store/observeStore.ts
@@ -1,11 +1,10 @@
 import {Store} from "redux";
-import {State} from "./state";
 
-export function observeStore<T>(store: Store<object>, select: (state: State) => T, onChange: (state: T) => void) {
+export function observeStore<T>(store: Store<object>, select: (state: any) => T, onChange: (state: T) => void) {
     let currentState: any;
 
     function handleChange() {
-        const nextState = select(store.getState() as State);
+        const nextState = select(store.getState());
         if (nextState !== currentState) {
             currentState = nextState;
             onChange(currentState);
